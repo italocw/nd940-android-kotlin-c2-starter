3cf26e51ee9d6666a081073d1336ea4c57f7cc0d
Ítalo Bezerra <italobezerra.cw@gmail.com>
2022-09-15 07:54:13 -0300

    Query to delete yesterday asteroids was write

--- starter/app/src/main/java/com/udacity/asteroidradar/AsteroidDatabaseDao.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/AsteroidDatabaseDao.kt
@@ -8,8 +8,11 @@ import androidx.room.Query
 @Dao
 interface AsteroidDatabaseDao {
    @Query("SELECT * FROM  asteroid_table where close_approach_date = date('now','localtime','start of day') ORDER BY close_approach_date DESC ")
-    fun getTodayAsteroids(): LiveData<List<Asteroid>>
+   suspend fun  getTodayAsteroids(): LiveData<List<Asteroid>>
 
     @Insert
     suspend fun insert(asteroid: Asteroid)
+
+    @Query("DELETE * FROM  asteroid_table where close_approach_date = date('now','localtime','start of day', '-1 day') ")
+    suspend fun  deleteYesterdayAsteroids()
 }
