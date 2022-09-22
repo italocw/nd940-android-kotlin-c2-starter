d58d0ecf748eecd4472843ea3805047193a5f420
Ítalo Bezerra <italobezerra.cw@gmail.com>
2022-09-22 12:04:01 -0300

    NASAs api key was removed

--- starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -1,7 +1,7 @@
 package com.udacity.asteroidradar
 
 object Constants {
-    const val API_KEY = "vjQJk8xv8NK68c3jC6hai2FzBM9zrlTYep3eIjRw"
+    const val API_KEY = "" //TODO: PUT YOUR OWN API_KEY HERE
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
